d5a26019767f0df1bb894ce10b4afb2e5cb526fb
Chukwuka Joseph Nwachukwu <83855667+Chukajoseph99@users.noreply.github.com>
2021-10-12 16:04:25 +0100

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,23 @@
+const hamburger = document.querySelector(".hamburger");
+const navList = document.querySelector(".nav-list");
+
+if(hamburger){
+hamburger.addEventListener("click", () => {
+navList.classList.toggle("open");
+});
+}
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 2000); // Change image every 2 seconds
+}
